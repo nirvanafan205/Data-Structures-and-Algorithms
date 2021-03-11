bed9b6e52fa022377311f4c5c139e1d87d8ec505
Matthew <mgarcia495@yahoo.com>
2021-03-11 00:34:54 -0800

    appended linked list

--- assignments/linked_list.cpp
+++ assignments/linked_list.cpp
@@ -4,14 +4,43 @@ using namespace std;
 
 class LL
 {
-	void appending();  //declaration
-	void inserting();
-	void deleting();
+	public:
+		void appending(LL** head, int new_info);  //declaration
+		void inserting();
+		void deleting();
+		void print(LL *node);
+
+		int data;
+		LL *next;
 };
 
-void LL::appending() //definitions
+void LL::appending(LL** head, int new_info) //definitions
 {
-	//do something
+	LL* allocating = new LL();
+
+	LL *last = *head;
+
+	allocating->data = new_info;
+
+	allocating->next = NULL;
+
+	if(*head == NULL)
+	{
+		*head = allocating;
+		return;
+	}
+
+	//traverse until the last node
+
+	while(last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	// change the next of the last node
+	last->next = allocating;
+	return;
+
 }
 
 void LL::inserting() // accessing member variables 
@@ -24,7 +53,26 @@ void LL::deleting()
 	// do something	
 } 
 
+void LL::print(LL *node)
+{
+	while(node != NULL)
+	{
+		cout << " " << node->data;
+		node = node->next;
+	}
+
+}
+
 int main()
 {
-	cout << "Hello World" << endl; //driver function
+	LL* head = NULL;
+	LL* caller;
+
+	caller->appending(&head, 7);
+	caller->appending(&head, 9);
+
+	cout << "Linked list appended: ";
+	caller->print(head);
+	cout << endl;
+
 }

0457757ee5e0b41bd268606dc12afc68805d21d3
Matthew <mgarcia495@yahoo.com>
2021-03-10 19:48:34 -0800

    linked list with appending, inserting, and deleting nodes functions

---
+++ assignments/linked_list.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class LL
+{
+	void appending();  //declaration
+	void inserting();
+	void deleting();
+};
+
+void LL::appending() //definitions
+{
+	//do something
+}
+
+void LL::inserting() // accessing member variables 
+{
+	//do something
+}
+
+void LL::deleting()
+{
+	// do something	
+} 
+
+int main()
+{
+	cout << "Hello World" << endl; //driver function
+}

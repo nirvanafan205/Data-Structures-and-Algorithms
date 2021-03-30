b691028abbd243c182bdce2a164350fcd1ed964f
Matthew <mgarcia495@yahoo.com>
2021-03-30 16:28:46 -0700

    made an object type of array of pointers, created passFail functions and called it from the main

--- assignments/projectx.cpp
+++ assignments/projectx.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+class projectx
+{
+	public:
+		void passFail(); // definition
+};
+
+
 int main()
+{
+	projectx* caller; // to call functions
+	
+	projectx* patients[] = {};
+
+	caller->passFail();
+}
+
+void projectx::passFail() // declaration
 {
 	cout << "Hello World" << endl;
 }

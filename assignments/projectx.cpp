e1d9bec58415765f7d6ad2219c35d553acebca96
Matthew <mgarcia495@yahoo.com>
2021-03-30 18:00:34 -0700

    added other functions and more variables

--- assignments/projectx.cpp
+++ assignments/projectx.cpp
@@ -4,22 +4,72 @@ using namespace std;
 
 class projectx
 {
-	public:
-		projectx passFail(); // definition
-};
+	private:
+		int labScore;
+		int grade;
+		int essayScore;
+		int finalScore;
 
+	public:
+		projectx passFail(projectx *nums[]); // definition
+		projectx finalExam(projectx *nums[]);
+		projectx essay(projectx *nums[]);
+		projectx lab(projectx *nums[]);
+
+		void setGrade(int score)
+		{
+			grade = score;
+		}
+
+		int getGrade()
+		{
+			return grade;
+		} 
+		projectx(int num)
+		{
+			grade = num;
+		}
+};
 
 int main()
 {
 	projectx* caller; // to call functions
-	
-	projectx* patients[] = {};
+	projectx pF(70);
 
-	caller->passFail();
+	projectx* patients[1] = {&pF}; //getting the addresss of pF
+
+	caller->passFail(patients); //calling passFail function to determine grade
+}
+
+projectx projectx::passFail(projectx *nums[]) // declaration
+{
+	for(int i = 0; i < 1; i++)
+	{
+		nums[i]->getGrade();
+
+		if(nums[i]->getGrade() > 69)
+		{
+			cout << "P" << endl;	
+		}
+
+		else
+		{
+			cout << "F" << endl;
+		}
+	}
+}
+
+projectx finalExam(projectx *nums[])
+{
 
 }
 
-projectx projectx::passFail() // declaration
+projectx essay(projectx *nums[])
 {
-	cout << "Hello World" << endl;
+
+}
+
+projectx lab(projectx *nums[])
+{
+
 }

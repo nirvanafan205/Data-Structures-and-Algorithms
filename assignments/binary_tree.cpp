556f0c23a061e8abb7113d343adf2427cbd229f9
Matthew <mgarcia495@yahoo.com>
2021-05-23 21:01:14 -0700

    added Perfect, Full, and Complete Binary Trees

--- assignments/binary_tree.cpp
+++ assignments/binary_tree.cpp
@@ -1,3 +1,11 @@
+/*
+   Matthew Garcia
+   CS 136 
+   Write a program that stores a series of numbers in a binary tree.
+   Then have the program insert the values into a queue in ascending order.
+   Dequeue the values and display them on the screen to confirm that they were stored in the proper order.
+*/
+
 #include <iostream>
 #include <queue>
 
@@ -25,8 +33,10 @@ void FtoL(queue<int> &q, int qsize);
 
 int main()
 {
-	queue<int> bst;
-	Nodes* root = create(27); //begining of tree
+	queue<int> Pbt, Fbt, Cbt;
+
+	//Perfect Binary Tree
+	Nodes* root = create(27); 
 
 	//left side of tree
 	root->left = create(8);
@@ -38,15 +48,69 @@ int main()
 	root->right->left = create(24);
 	root->right->right = create(57);
 
-	//calling to print binary tree inorder
-	inOrder(root, bst);
-	sort(bst);
+	//inputs binary tree values into queue
+	//it is then sorted in ascending order
+	inOrder(root, Pbt);
+	sort(Pbt);
 
-	while(!bst.empty() )
+	//output
+	cout << "Perfect Binary Tree is printed in ascending order: ";
+	while(!Pbt.empty() )
 	{
-		cout << bst.front() << " ";
-		bst.pop();
+		cout << Pbt.front() << " ";
+		Pbt.pop();
 	}
+
+	cout << endl;
+
+	//Full Binary Tree
+	Nodes* rootF = create(9);  
+
+	//Only node of left side 
+	rootF->left = create(42);
+
+	//right side filled
+	rootF->right = create(82);
+	rootF->right->left = create(21);
+	rootF->right->right = create(1);
+
+	inOrder(rootF, Fbt);
+	sort(Fbt);
+
+	cout << "Full Binary Tree is printed in ascending order: ";
+	while(!Fbt.empty() )
+	{
+		cout << Fbt.front() << " ";
+		Fbt.pop();
+	}
+	cout << endl;
+
+	//Complete Binary Tree
+	Nodes* rootC = create(123); //last level isn't filled completely 
+
+	//left side filled
+	rootC->left = create(23);
+	rootC->left->right = create(1);
+	rootC->left->right->left = create(14);
+	rootC->left->left = create(11);
+	rootC->left->left->left = create(9);
+	rootC->left->left->right = create(2);
+
+	//right side partially filled
+	rootC->right = create(30);
+	rootC->right->left = create(42);
+	rootC->right->right = create(9);
+
+	inOrder(rootC, Cbt);
+	sort(Cbt);
+
+	cout << "Complete Binary Tree is printed in ascending order: ";
+	while(!Cbt.empty() )
+	{
+		cout << Cbt.front() << " ";
+		Cbt.pop();
+	}
+	cout << endl;
 }
 
 Nodes* create(int data)
@@ -76,7 +140,6 @@ void inOrder(Nodes* temp, queue<int> &tree)
 
 void sort(queue<int> &q)
 {
-
 	if(q.empty() )
 	{
 		return;
@@ -91,7 +154,6 @@ void sort(queue<int> &q)
 	push(q, temp, q.size() );
 }
 
-
 void push(queue<int> &q, int temp, int size)
 {
 	if(q.empty() || size == 0)
@@ -103,7 +165,6 @@ void push(queue<int> &q, int temp, int size)
 	else if(temp <= q.front() )
 	{
 		q.push(temp);
-
 
 		FtoL(q, size);
 	}
